98cd5e18fb78056074f74e732a252be56af39081
Globox1997 <mitschke11@web.de>
2021-08-06 13:31:07 +0200

    added max health bonus

--- src/main/java/net/levelz/mixin/player/HungerManagerMixin.java
+++ src/main/java/net/levelz/mixin/player/HungerManagerMixin.java
@@ -4,10 +4,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.levelz.access.PlayerStatsManagerAccess;
 import net.levelz.init.ConfigInit;
+import net.levelz.stats.PlayerStatsManager;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
 
@@ -19,7 +21,18 @@ public class HungerManagerMixin {
     private float foodSaturationLevel;
 
     @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;heal(F)V", ordinal = 1))
-    private void updateMixin(PlayerEntity player, CallbackInfo info) {
-        player.heal((float) ((PlayerStatsManagerAccess) player).getPlayerStatsManager(player).getLevel("stamina") * ConfigInit.CONFIG.staminaHealthBonus);
+    private void updateStaminaMixin(PlayerEntity player, CallbackInfo info) {
+        PlayerStatsManager playerStatsManager = ((PlayerStatsManagerAccess) player).getPlayerStatsManager(player);
+        player.heal((float) playerStatsManager.getLevel("stamina") * ConfigInit.CONFIG.staminaHealthBonus);
+
+    }
+
+    @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;addExhaustion(F)V", shift = Shift.AFTER, ordinal = 1))
+    private void updateAbsorptionMixin(PlayerEntity player, CallbackInfo info) {
+        PlayerStatsManager playerStatsManager = ((PlayerStatsManagerAccess) player).getPlayerStatsManager(player);
+        if (player.getMaxHealth() <= player.getHealth() && player.getAbsorptionAmount() <= 0.0F && playerStatsManager.getLevel("health") == ConfigInit.CONFIG.maxLevel) {
+
+            player.setAbsorptionAmount(ConfigInit.CONFIG.healthAbsorptionBonus);
+        }
     }
 }
