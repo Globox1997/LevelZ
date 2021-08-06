1f0f17972c0532e59fd603c23c72cc0df331e1f0
Globox1997 <mitschke11@web.de>
2021-08-06 10:46:47 +0200

    added tnt mixin

---
+++ src/main/java/net/levelz/mixin/entity/TntEntityMixin.java
@@ -0,0 +1,40 @@
+package net.levelz.mixin.entity;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.levelz.access.PlayerStatsManagerAccess;
+import net.levelz.init.ConfigInit;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+
+@Mixin(TntEntity.class)
+public abstract class TntEntityMixin extends Entity {
+
+    @Shadow
+    @Nullable
+    private LivingEntity causingEntity;
+
+    public TntEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "explode", at = @At(value = "HEAD"), cancellable = true)
+    private void explodeMixin(CallbackInfo info) {
+        if (causingEntity != null && causingEntity instanceof PlayerEntity) {
+            if (((PlayerStatsManagerAccess) (PlayerEntity) causingEntity).getPlayerStatsManager((PlayerEntity) causingEntity).getLevel("mining") == ConfigInit.CONFIG.maxLevel) {
+                this.world.createExplosion(this, this.getX(), this.getBodyY(0.0625D), this.getZ(), 4.0F * ConfigInit.CONFIG.miningTntBonus, Explosion.DestructionType.BREAK);
+                info.cancel();
+            }
+        }
+    }
+}
