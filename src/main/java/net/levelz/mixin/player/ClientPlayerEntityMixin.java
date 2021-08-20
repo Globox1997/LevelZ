56d39a22175e09343ef4903defee2e8a356901f5
Globox1997 <mitschke11@web.de>
2021-08-20 10:42:57 +0200

    flush

--- src/main/java/net/levelz/mixin/player/ClientPlayerEntityMixin.java
+++ src/main/java/net/levelz/mixin/player/ClientPlayerEntityMixin.java
@@ -3,10 +3,17 @@ package net.levelz.mixin.player;
 import net.fabricmc.api.Environment;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.At;
 
 import net.fabricmc.api.EnvType;
-
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.recipebook.ClientRecipeBook;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.stat.StatHandler;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ClientPlayerEntity.class)
@@ -14,8 +21,9 @@ public class ClientPlayerEntityMixin {
 
     // No use yet
 
-    // @Inject(method = "<init>", at = @At(value = "TAIL"))
-    // private void initMixin(MinecraftClient client, ClientWorld world, ClientPlayNetworkHandler networkHandler, StatHandler stats, ClientRecipeBook recipeBook, boolean lastSneaking,
-    // boolean lastSprinting, CallbackInfo info) {
-    // }
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    private void initMixin(MinecraftClient client, ClientWorld world, ClientPlayNetworkHandler networkHandler, StatHandler stats, ClientRecipeBook recipeBook, boolean lastSneaking,
+            boolean lastSprinting, CallbackInfo info) {
+        // System.out.println("client::" + client.player.getHealth());
+    }
 }
