f2ed998709f409589cf8e3560851dc634f10e1fc
xFusionLordx <FusionLord@live.com>
2021-09-04 07:56:57 -0700

    Render red outline if unable to mine block.

---
+++ src/main/java/net/levelz/mixin/render/WorldRendererMixin.java
@@ -0,0 +1,40 @@
+package net.levelz.mixin.render;
+
+import net.levelz.stats.PlayerStatsManager;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public abstract class WorldRendererMixin {
+    @Shadow
+    private ClientWorld world;
+
+    @Shadow
+    private static void drawShapeOutline(MatrixStack matrices, VertexConsumer vertexConsumer, VoxelShape voxelShape, double d, double e, double f, float g, float h, float i, float j)
+    {
+        throw new AbstractMethodError("shadow");
+    }
+
+    @Inject(method = "drawBlockOutline(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/entity/Entity;DDDLnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V", at = @At(value = "HEAD"), cancellable = true)
+    private void drawBlockOutline(MatrixStack matrices, VertexConsumer vertexConsumer, Entity entity, double d, double e, double f, BlockPos blockPos, BlockState blockState, CallbackInfo ci)
+    {
+        if (PlayerStatsManager.listContainsItemOrBlock(MinecraftClient.getInstance().player, Registry.BLOCK.getRawId(blockState.getBlock()), true)) {
+            drawShapeOutline(matrices, vertexConsumer, blockState.getOutlineShape(this.world, blockPos, ShapeContext.of(entity)), (double) blockPos.getX() - d, (double) blockPos.getY() - e, (double) blockPos.getZ() - f, 1.0F, 0.0F, 0.0F, 0.4F);
+            ci.cancel();
+        }
+    }
+}
