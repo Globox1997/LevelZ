d4310654f6033a1a8e502921b443d3478aba349c
Globox1997 <mitschke11@web.de>
2021-10-05 11:47:56 +0200

    updated dependencies

--- src/main/java/net/levelz/init/TagInit.java
+++ src/main/java/net/levelz/init/TagInit.java
@@ -1,13 +1,13 @@
 package net.levelz.init;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class TagInit {
 
-    public static final Tag<Item> FARM_ITEMS = TagRegistry.item(new Identifier("levelz", "farm_items"));
+    public static final Tag<Item> FARM_ITEMS = TagFactory.ITEM.create(new Identifier("levelz", "farm_items"));
 
     public static void init() {
 
