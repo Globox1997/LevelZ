e177c8e9369c4743ebd1e3fa613fcd96bd330a3b
Globox1997 <mitschke11@web.de>
2022-06-11 16:32:32 +0200

    added rare candy

--- src/main/java/net/levelz/init/ItemInit.java
+++ src/main/java/net/levelz/init/ItemInit.java
@@ -23,9 +23,11 @@ import net.minecraft.util.registry.Registry;
 public class ItemInit {
 
     public static final Item STRANGE_POTION = new StrangePotionItem(new Item.Settings().group(ItemGroup.BREWING).maxCount(1));
+    public static final Item RARE_CANDY = new RareCandyItem(new Item.Settings().group(ItemGroup.MISC));
 
     public static void init() {
         Registry.register(Registry.ITEM, new Identifier("levelz", "strange_potion"), STRANGE_POTION);
+        Registry.register(Registry.ITEM, new Identifier("levelz", "rare_candy"), RARE_CANDY);
 
         UseItemCallback.EVENT.register((player, world, hand) -> {
             if (!player.isCreative() && !player.isSpectator()) {
---
+++ src/main/java/net/levelz/item/RareCandyItem.java
@@ -0,0 +1,32 @@
+package net.levelz.item;
+
+import net.levelz.access.PlayerStatsManagerAccess;
+import net.levelz.access.PlayerSyncAccess;
+import net.levelz.stats.PlayerStatsManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+// Texture made by Pois1x
+public class RareCandyItem extends Item {
+
+    public RareCandyItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        if (!world.isClient) {
+            if (!user.isCreative())
+                stack.decrement(1);
+            PlayerStatsManager playerStatsManager = ((PlayerStatsManagerAccess) user).getPlayerStatsManager(user);
+            ((PlayerSyncAccess) user).addLevelExperience(playerStatsManager.getNextLevelExperience() - ((int) (playerStatsManager.levelProgress * playerStatsManager.getNextLevelExperience())));
+        }
+        return TypedActionResult.success(stack, world.isClient());
+    }
+
+}
