692fcc7db98cec496e4d69f3eadb1b415d50b9bd
Globox1997 <mitschke11@web.de>
2022-06-13 10:47:10 +0200

    added bucketable restriction

---
+++ src/main/java/net/levelz/mixin/misc/BucketableMixin.java
@@ -0,0 +1,32 @@
+package net.levelz.mixin.misc;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.levelz.data.LevelLists;
+import net.levelz.stats.PlayerStatsManager;
+import net.minecraft.entity.Bucketable;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+
+@Mixin(Bucketable.class)
+public interface BucketableMixin {
+
+    @Inject(method = "tryBucket", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;playSound(Lnet/minecraft/sound/SoundEvent;FF)V"))
+    private static <T extends LivingEntity> void tryBucket(PlayerEntity player, Hand hand, T entity, CallbackInfoReturnable<Optional<ActionResult>> info) {
+        ArrayList<Object> levelList = LevelLists.bucketList;
+        if (!PlayerStatsManager.playerLevelisHighEnough(player, levelList, null, true)) {
+            player.sendMessage(Text.translatable("item.levelz." + levelList.get(0) + ".tooltip", levelList.get(1)).formatted(Formatting.RED), true);
+            info.setReturnValue(Optional.empty());
+        }
+    }
+}
