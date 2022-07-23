0c55f6b88bdd6bb7e05831a065f735c28f681a14
Globox1997 <mitschke11@web.de>
2022-07-23 09:40:27 +0200

    added strange potion rei compat

--- src/main/java/net/levelz/compat/LevelzReiPlugin.java
+++ src/main/java/net/levelz/compat/LevelzReiPlugin.java
@@ -2,19 +2,31 @@ package net.levelz.compat;
 
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.client.registry.screen.DisplayBoundsProvider;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import me.shedaniel.rei.plugin.common.displays.brewing.DefaultBrewingDisplay;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.levelz.gui.InfoScreen;
 import net.levelz.gui.LevelzScreen;
+import net.levelz.init.ItemInit;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 
 import org.jetbrains.annotations.Nullable;
 
 @Environment(EnvType.CLIENT)
 public class LevelzReiPlugin implements REIClientPlugin {
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        registry.add(new DefaultBrewingDisplay(EntryIngredients.of(Items.DRAGON_BREATH), EntryIngredients.of(Items.NETHER_STAR), EntryStacks.of(new ItemStack(ItemInit.STRANGE_POTION))));
+    }
 
     @Override
     public void registerScreens(ScreenRegistry registry) {
