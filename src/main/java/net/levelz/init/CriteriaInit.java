9c5c860bd6771e0edd5924b8d43263857b3be8a5
James (Jamalam) <james@jamalam.tech>
2023-07-09 15:18:33 +0100

    1.20.1, fix usage of fabric impl
    - update mod to build on 1.20.1. no changes required
    - remove random placeholder suggests in f.m.j
    - use TAW rather than fabric api impl detail in CriteriaInit
    
    note that the minecraft dependency is hardcoded to >=1.20 since it doesn't make sense to have it set to `1.20.1` (`${minecraft_version}`), as the mod works on 1.20 and 1.20.1

--- src/main/java/net/levelz/init/CriteriaInit.java
+++ src/main/java/net/levelz/init/CriteriaInit.java
@@ -1,16 +1,15 @@
 package net.levelz.init;
 
-import net.fabricmc.fabric.mixin.object.builder.CriteriaAccessor;
 import net.levelz.criteria.LevelZCriterion;
 import net.levelz.criteria.SkillCriterion;
-import net.levelz.mixin.misc.ScoreboardCriterionAccessor;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.scoreboard.ScoreboardCriterion;
 
 public class CriteriaInit {
 
-    public static final LevelZCriterion LEVEL_UP = CriteriaAccessor.callRegister(new LevelZCriterion());
-    public static final SkillCriterion SKILL_UP = CriteriaAccessor.callRegister(new SkillCriterion());
-    public static final ScoreboardCriterion LEVELZ = ScoreboardCriterionAccessor.callCreate("levelz");
+    public static final LevelZCriterion LEVEL_UP = Criteria.register(new LevelZCriterion());
+    public static final SkillCriterion SKILL_UP = Criteria.register(new SkillCriterion());
+    public static final ScoreboardCriterion LEVELZ = ScoreboardCriterion.create("levelz");
 
     public static void init() {
     }
