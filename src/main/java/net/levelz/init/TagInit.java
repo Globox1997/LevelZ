4a2084a936c189e4d8a9b7f65c2daee47b270ced
Globox1997 <Micha.Damaschke@web.de>
2026-01-07 13:25:26 +0100

    flush

--- src/main/java/net/levelz/init/TagInit.java
+++ src/main/java/net/levelz/init/TagInit.java
@@ -10,6 +10,7 @@ public class TagInit {
 
     public static final TagKey<Item> RESTRICTED_FURNACE_EXPERIENCE_ITEMS = TagKey.of(RegistryKeys.ITEM, LevelzMain.identifierOf("restricted_furnace_experience_items"));
     public static final TagKey<Block> RESTRICTED_ORE_EXPERIENCE_BLOCKS = TagKey.of(RegistryKeys.BLOCK, LevelzMain.identifierOf("restricted_ore_experience_blocks"));
+
     public static void init() {
     }
 }
